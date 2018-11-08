73c1e1e1f0ab30bf136493b688f87582a22460d4
SantiagoCabrales <sacamo96@gmail.com>
2018-11-08 05:46:35 -0500

    LeerBD casi

--- app/src/main/java/oh/javeriana/co/oh/AgregarAlojamientoActivity.java
+++ app/src/main/java/oh/javeriana/co/oh/AgregarAlojamientoActivity.java
@@ -18,8 +18,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import android.location.Geocoder;
@@ -187,6 +190,8 @@ public class AgregarAlojamientoActivity extends Activity {
             return false;
         }
     }
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
